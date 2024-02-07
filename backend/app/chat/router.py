53c9de0a415d1535944d118fac69c2eb3445966d
t0r0id <bhoumiks5910@gmail.com>
2024-02-07 21:07:52 +0530

    stream chat response

--- backend/app/chat/router.py
+++ backend/app/chat/router.py
@@ -3,10 +3,12 @@ logger = logging.getLogger(__name__)
 
 from fastapi import APIRouter, HTTPException
 from click import UUID
-from typing import List
+from typing import Generator, List
+from sse_starlette.sse import EventSourceResponse
+from llama_index.core.llms.types import MessageRole
 
-from app.db.models import ChatMessage, Chat
-from app.chat.engine import create_new_chat, get_chat_history, generate_chat_response
+from app.db.models import ChatResponse, Chat, ChatResponseStatusEnum
+from app.chat.engine import create_new_chat, get_chat_history, generate_chat_response_stream
 
 chat_router = APIRouter()
 
@@ -28,7 +30,7 @@ async def initiate(channel_id: str) -> str:
         HTTPException(status_code=500, detail="chat creation failed")
 
 @chat_router.get("/history/")
-async def chat_history(chat_id: str) -> List[ChatMessage]:
+async def chat_history(chat_id: str) -> List[ChatResponse]:
     """
     Retrieves the chat history for the specified chat ID.
 
@@ -43,29 +45,83 @@ async def chat_history(chat_id: str) -> List[ChatMessage]:
         return chat_history
     except Exception as e:
         raise HTTPException(status_code=500, detail="chat history retrieval failed")
-
-@chat_router.post("/message/")
-async def message(chat_id: str, user_message: str) -> List[ChatMessage]:
+    
+@chat_router.post("/message_stream/")
+async def message_stream(chat_id: str, user_message: str) -> EventSourceResponse:
     """
-    Saves the chat message to the database and generates a response.
-
+    Endpoint for streaming chat responses.
+    
     Args:
-        chat_id (str): The ID of the chat.
-        user_message (str): The chat message to save.
+        chat_id: The ID of the chat.
+        user_message: The message from the user.
     
     Returns:
-        List[ChatMessage]: The generated chat response.
+        EventSourceResponse: The response stream.
     """
     try:
-        # Get the chat from the database using the chat_id
+        # Get the chat by ID
         chat = await Chat.get(chat_id)
-        
-        # If chat is not found, raise a ValueError
+
+        # If chat not found, raise an error
         if not chat:
             logger.error(f"Chat not found for chat {chat_id}")
             raise HTTPException(status_code=400, detail=f"Chat not found for chat {chat_id}")
+        
+        # Generate the chat response stream
+        stream = await generate_chat_response_stream(chat, user_message)
+        
+        # If stream not generated, add failed response to chat history and raise an error
+        if not stream:
+            chat.chat_history.append(ChatResponse(role=MessageRole.USER
+                                                  , content=user_message
+                                                  , status=ChatResponseStatusEnum.FAILED
+                                                  , error="No stream generated"
+                                                  ))
+            await chat.save()
+            raise HTTPException(status_code=500, detail="chat response stream generation failed")
+        
+        # Initialize the chat response
+        chat_response = ChatResponse(role=MessageRole.ASSISTANT, content="", status=ChatResponseStatusEnum.IN_PROGRESS)
+        
+        # Define the stream response generator
+        async def stream_response(chat) -> Generator[ChatResponse, None, None]:
+            async for delta in stream.async_response_gen():
+                chat_response.content += delta
+                yield chat_response
+            chat_response.status = ChatResponseStatusEnum.COMPLETED
+            chat.chat_history.append(ChatResponse(role=MessageRole.USER,
+                                                  content=user_message,
+                                                  status=ChatResponseStatusEnum.COMPLETED
+                                                  )
+                                                  )
+            chat.chat_history.append(chat_response)
+            chat = await chat.save()
+            yield chat_response
+        return EventSourceResponse(stream_response(chat))
+    except Exception as e:
+        logger.error(f"Failed to generate chat response stream for chat {chat_id}", e)
+        raise HTTPException(status_code=500, detail="chat response generation failed")
 
-        chat_history = await generate_chat_response(chat, user_message)
-        return chat_history
+@chat_router.post("/message/")
+async def message(chat_id: str, user_message: str) -> ChatResponse:
+    """
+    Endpoint to handle incoming chat messages and generate a response without stream.
+
+    Args:
+    - chat_id (str): The ID of the chat session.
+    - user_message (str): The message sent by the user.
+
+    Returns:
+    - ChatResponse: The response generated for the user message.
+    """
+    try:
+        response: EventSourceResponse = await message_stream(chat_id, user_message)
+        final_message = None
+        async for message in response.body_iterator:
+            final_message = message
+        if final_message is not None:
+            return final_message
+        else:
+            raise HTTPException(status_code=500, detail="chat response generation failed")
     except:
         raise HTTPException(status_code=500, detail="chat response generation failed")
