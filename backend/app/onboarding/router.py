859fd5f45728ee4768b89c62a7343e5aa376aa1c
t0r0id <bhoumiks5910@gmail.com>
2024-02-09 13:50:31 +0530

    Use request object in APIs

--- backend/app/onboarding/router.py
+++ backend/app/onboarding/router.py
@@ -2,8 +2,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 from typing import Optional, List
-from fastapi import APIRouter, HTTPException
-from click import UUID 
+from fastapi import APIRouter, HTTPException, Request, Body
 
 from app.onboarding.engine import create_onboarding_request, process_onboarding_request, search_for_channels
 from app.db.models import Channel, ChannelOnBoardingRequest, ChannelOnBoardingRequestStatusEnum
@@ -11,7 +10,9 @@ from app.db.models import Channel, ChannelOnBoardingRequest, ChannelOnBoardingRe
 onboard_router = APIRouter()
 
 @onboard_router.get("/search_channels/")
-async def search_channels(query: str, region: Optional[str]='US', limit: Optional[int]=5) -> List[Channel]:
+async def search_channels(query: str
+                          , region: Optional[str]='US'
+                          , limit: Optional[int]= 5) -> List[Channel]:
     """
     Endpoint to search for channel videos
     
@@ -21,7 +22,7 @@ async def search_channels(query: str, region: Optional[str]='US', limit: Optiona
     - limit: Optional[int], the maximum number of results to return
     
     Returns:
-    - dict: channel videos matching the search query
+    - List[Channel]: channel videos matching the search query
     """
     try:
         return await search_for_channels(query, region, limit)
@@ -29,11 +30,14 @@ async def search_channels(query: str, region: Optional[str]='US', limit: Optiona
         raise HTTPException(status_code=500, detail="channel search failed")
 
 @onboard_router.post("/initiate_request")
-async def initiate_request(channel_id: str, requested_by: str) -> ChannelOnBoardingRequest:
+async def initiate_request(request: Request,
+                           channel_id: str= Body(..., embed=True),
+                           requested_by: str= Body(..., embed=True)) -> ChannelOnBoardingRequest:
     """
     An asynchronous function to handle the onboarding request for a specific channel.
     
     Parameters:
+    - request: the incoming request object
     - channel_id: a string representing the ID of the channel
     - requested_by: a string representing the ID of the user who requested the onboarding
     
@@ -47,43 +51,43 @@ async def initiate_request(channel_id: str, requested_by: str) -> ChannelOnBoard
         raise HTTPException(status_code=500, detail="request creation failed")
 
 @onboard_router.post("/process_request")
-async def process_request(request_id: str) -> dict:
+async def process_request(request: Request,
+                          request_id: str = Body(..., embed=True)) -> dict:
     """
-    A function to process an onboarding request identified by the request_id parameter and returns a dictionary with a message and the request_id.
-    
+    Process an onboarding request and returns a dictionary with a message and the request_id.
+
     Parameters:
     - request_id: a string representing the ID of the onboarding request
-    
+
     Returns:
     - a dictionary with keys "message" and "request_id"
     """
-    #TODO: Restrict access only to admins
+    # TODO: Restrict access only to admins
 
     # Retrieve the onboarding request by ID
-    request = await ChannelOnBoardingRequest.get(request_id)
+    onboarding_request = await ChannelOnBoardingRequest.get(request_id)
 
     # Check if the request exists
-    if not request:
+    if not onboarding_request:
         raise HTTPException(status_code=404, detail=f"Onboarding request ({request_id}) not found")
 
     # Check if the request is in the QUEUED state
-    if request.status != ChannelOnBoardingRequestStatusEnum.QUEUED:
-        raise HTTPException(status_code=404, detail=f"Onboarding request ({request.id}) is not in the QUEUED state. Request status: {request.status}")
+    if onboarding_request.status != ChannelOnBoardingRequestStatusEnum.QUEUED:
+        raise HTTPException(status_code=404, detail=f"Onboarding request ({onboarding_request.id}) is not in the QUEUED state. Request status: {onboarding_request.status}")
 
     # Check if the channel has already been onboarded
-    channel = Channel.find_one(Channel.id == request.channel_id)
-    if await channel:
-        request.status = ChannelOnBoardingRequestStatusEnum.COMPLETED
-        await request.save()
-        raise HTTPException(status_code=404
-                            , detail=f"Channel {request.channel_id} for request {request.id} has already been onboarded. Channel status: {channel.status}"
-                            )
+    channel = await Channel.find_one(Channel.id == onboarding_request.channel_id)
+    if channel:
+        onboarding_request.status = ChannelOnBoardingRequestStatusEnum.COMPLETED
+        await onboarding_request.save()
+        raise HTTPException(status_code=404, detail=f"Channel {onboarding_request.channel_id} for request {onboarding_request.id} has already been onboarded. Channel status: {channel.status}")
+
     try:
-        await process_onboarding_request(request)
+        await process_onboarding_request(onboarding_request)
         return {
             "message": f"Onboarding request successfully processed!",
             "request_id": request_id
-            }
+        }
     except Exception as e:
-        raise HTTPException(status_code=500, detail="request processing failed")
+        raise HTTPException(status_code=500, detail="Request processing failed")
     
