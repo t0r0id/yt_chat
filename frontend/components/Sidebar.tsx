04a4b1133ceee9aea6dbfa9db8f67eedf6546627
t0r0id <bhoumiks5910@gmail.com>
2024-02-11 16:01:04 +0530

    enable channel removal

--- frontend/components/Sidebar.tsx
+++ frontend/components/Sidebar.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-import { Plus } from "lucide-react";
+import { Plus, Trash2 } from "lucide-react";
 
 import { ChannelType } from "@/lib/types/yt";
 import { YtApiClient } from "@/lib/api/yt";
@@ -17,15 +17,23 @@ const Sidebar = (props: Props) => {
   const pathName = usePathname();
   useEffect(() => {
     (async () => {
-      const { data } = await YtApiClient.getChannels();
+      const { data } = await YtApiClient.getUserChannels();
       if (data) {
         const channels = data as ChannelType[];
-        console.log(channels[0]);
-        console.log(pathName.split("/").at(-1));
         setChannels(channels);
       }
     })();
   }, []);
+
+  const handleChannelRemoval = async (channelId: string) => {
+    const response = await YtApiClient.removeUserChannel(channelId);
+    if (response) {
+      const newChannels = channels.filter(
+        (channel) => channel._id !== channelId
+      );
+      setChannels([...newChannels]);
+    }
+  };
 
   return (
     <div className="flex flex-col h-full w-full">
@@ -45,30 +53,38 @@ const Sidebar = (props: Props) => {
       </div>
       <div className="flex-grow overflow-y-auto mx-2 pb-2 space-y-1">
         {channels.map((channel) => (
-          <Link
-            href={`/conversation/c/${channel._id}`}
-            key={channel._id}
-            className={cn(
-              `text-md group flex p-3 w-full 
+          <div className="relative group" key={channel._id}>
+            <Link
+              href={`/conversation/c/${channel._id}`}
+              className={cn(
+                `text-md group flex p-3 w-full 
             justify-start font-medium cursor-pointer
             hover:text-white hover:bg-white/10 rounded-lg transition-colors duration-200
             `,
-              pathName.split("/").at(-1) === channel._id
-                ? "bg-white/10 text-white"
-                : "text-inherit"
-            )}
-          >
-            <div className="flex items-center flex-1">
-              <img
-                src={channel.thumbnails?.[0]?.url?.toString()}
-                alt="Channel Thumbnail"
-                className="mr-2"
-              />
-              <div>
+                pathName.split("/").at(-1) === channel._id
+                  ? "bg-white/10 text-white"
+                  : "text-inherit"
+              )}
+            >
+              <div className="flex items-center flex-1">
+                <img
+                  src={channel.thumbnails?.[0]?.url?.toString()}
+                  alt="Channel Thumbnail"
+                  className="mr-2"
+                />
                 <div>{channel.title}</div>
               </div>
-            </div>
-          </Link>
+            </Link>
+            <Button
+              className="absolute right-1 top-1 p-1
+            text-inherit space-x-1 bg-inherit
+            hover:text-red-500
+            rounded-md group-hover:block"
+              onClick={() => handleChannelRemoval(channel._id)}
+            >
+              <Trash2 className="h-3 w-3" />
+            </Button>
+          </div>
         ))}
       </div>
     </div>
